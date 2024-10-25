55906c33b6da1b9d309fab6c77d0fcb60c4f69fa
unknown <martin.kenirons@gmit.ie>
2024-10-25 13:43:26 +0000

    exercise 4 complete

--- server.js
+++ server.js
@@ -2,9 +2,19 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+
 app.get('/', (req, res) => {
     res.send('Welcome to Data Respresentation & Querying');
 });
+
+app.get('/hello/:name/:lname', (req, res)=>{
+    res.send("Hello "+req.params.name+" "+req.params.lname);
+})
+
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
